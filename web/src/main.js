96a5add72185f3ae9509a2065fc1e324e454a513
dongwenjuan <1030843117@qq.com>
2019-03-04 19:52:37 +0800

    app和main文件提交

---
+++ web/src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Element from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
+import "./assets/css/reset.less";
+import "./assets/css/style.less";
+
+Vue.use(Element);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
